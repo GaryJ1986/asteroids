3a9c2ccdde3e25dcbd5fff8b7d67fe34283e02de
GaryJ1986 <garyjessop@gmail.com>
2024-08-19 11:20:51 +0100

    updated player

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ import pygame
 # import everything from a file
 from constants import *
 
+#import specific bits from a file
+from player import Player
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 )
     dt = 0
 
     while True:
@@ -17,12 +21,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(screen, (0,0,0))
+        screen.fill("black")
         pygame.display.flip()
 
         # Frame Limiter
-        dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000   
     
-
 if __name__ == "__main__":
     main()
