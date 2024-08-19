942c40509492a5202d94325a1f89b4c6c1fe38bd
GaryJ1986 <garyjessop@gmail.com>
2024-08-19 12:01:34 +0100

    added asteroid fields

--- main.py
+++ main.py
@@ -8,11 +8,22 @@ from constants import *
 
 #import specific bits from a file
 from player import Player
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (updateable, drawable, asteroids)
+    AsteroidField.containers = (updateable)
+    asteroid_field = AsteroidField()
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 )
     dt = 0
 
@@ -20,11 +31,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        player.update(dt)
+        
+        for obj in updateable:
+            obj.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
 
         # Frame Limiter
